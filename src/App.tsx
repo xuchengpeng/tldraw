81bf3353b1788936bd1b297a5502be65496dd90d
xuchengpeng <330476629@qq.com>
2024-07-31 10:49:00 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Tldraw } from 'tldraw'
+import './App.css'
+
+function App() {
+	return (
+		<div style={{ position: 'fixed', inset: 0 }}>
+			<Tldraw />
+		</div>
+	)
+}
+
+export default App
